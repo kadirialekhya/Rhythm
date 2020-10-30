2fe02bf10ab1a35e87a2529f06327c0c63815062
DESKTOP-SHALINI\user <siddabathula.s@northeastern.edu>
2020-10-30 16:10:31 -0700

    Added Business files

---
+++ Assignment4/src/Business/CustomerDirectory.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Shalini
+ */
+public class CustomerDirectory {
+    private ArrayList<Customer>customerList;
+
+     public CustomerDirectory() {
+        
+        customerList = new ArrayList<>();
+        
+    }
+     
+    public ArrayList<Customer> getCustomerDirectory() {
+        return customerList;
+    }
+
+    public void setCustomerDirectory(ArrayList<Customer> customerDirectory) {
+        this.customerList = customerDirectory;
+    }
+
+   public Customer addCustomer(String customerName,long customerContact, String customerEmail,String flightNumber, String seatNumber) {
+       Customer customer = new Customer(customerName , customerContact , customerEmail, flightNumber, seatNumber );
+       customerList.add(customer);
+       return customer;
+   }
+   
+   public void deleteCustomer(Customer customer) {
+        customerList.remove(customer);
+    }
+}
